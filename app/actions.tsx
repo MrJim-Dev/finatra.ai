8d1723c38aeccaabdc11fce4ea60c186a293e8a9
MrJim Development <mrjim.development@gmail.com>
2024-08-03 19:44:10 +0800

    uncomment other functions

--- app/actions.tsx
+++ app/actions.tsx
@@ -142,71 +142,71 @@ export const AI = createAI({
   }
 })
 
-// export async function getChats(id: string) {
-//   const supabase = createClient()
-//   const { data } = await supabase
-//     .from('chats')
-//     .select('payload')
-//     .eq('id', id)
-//     .maybeSingle()
+export async function getChats(id: string) {
+  const supabase = createClient()
+  const { data } = await supabase
+    .from('chats')
+    .select('payload')
+    .eq('id', id)
+    .maybeSingle()
 
-//   return (data?.payload as Chat) ?? null
-// }
+  return (data?.payload as Chat) ?? null
+}
 
-// export async function removeChat({ id, path }: { id: string; path: string }) {
-//   try {
-//     const supabase = createClient()
+export async function removeChat({ id, path }: { id: string; path: string }) {
+  try {
+    const supabase = createClient()
 
-//     await supabase.from('chats').delete().eq('id', id).throwOnError()
+    await supabase.from('chats').delete().eq('id', id).throwOnError()
 
-//     revalidatePath('/')
-//     return revalidatePath(path)
-//   } catch (error) {
-//     return {
-//       error: 'Unauthorized'
-//     }
-//   }
-// }
+    revalidatePath('/')
+    return revalidatePath(path)
+  } catch (error) {
+    return {
+      error: 'Unauthorized'
+    }
+  }
+}
 
-// export async function clearChats() {
-//   try {
-//     const supabase = createClient()
+export async function clearChats() {
+  try {
+    const supabase = createClient()
 
-//     await supabase.from('chats').delete().throwOnError()
-//     revalidatePath('/')
-//     return redirect('/')
-//   } catch (error) {
-//     console.log('clear chats error', error)
-//     return {
-//       error: 'Unauthorized'
-//     }
-//   }
-// }
+    await supabase.from('chats').delete().throwOnError()
+    revalidatePath('/')
+    return redirect('/')
+  } catch (error) {
+    console.log('clear chats error', error)
+    return {
+      error: 'Unauthorized'
+    }
+  }
+}
 
-// export async function getSharedChat(id: string) {
-//   const supabase = createClient()
-//   const { data } = await supabase
-//     .from('chats')
-//     .select('payload')
-//     .eq('id', id)
-//     .not('payload->sharePath', 'is', null)
-//     .maybeSingle()
+export async function getSharedChat(id: string) {
+  const supabase = createClient()
+  const { data } = await supabase
+    .from('chats')
+    .select('payload')
+    .eq('id', id)
+    .not('payload->sharePath', 'is', null)
+    .maybeSingle()
 
-//   return (data?.payload as Chat) ?? null
-// }
+  return (data?.payload as Chat) ?? null
+}
 
-// export async function shareChat(chat: Chat) {
-//   const payload = {
-//     ...chat,
-//     sharePath: `/share/${chat.id}`
-//   }
+export async function shareChat(chat: Chat) {
+  const payload = {
+    ...chat,
+    sharePath: `/share/${chat.id}`
+  }
 
-//   const supabase = createClient()
-//   await supabase
-//     .from('chats')
-//     .update({ payload: payload as any })
-//     .eq('id', chat.id)
-//     .throwOnError()
+  const supabase = createClient()
+  await supabase
+    .from('chats')
+    .update({ payload: payload as any })
+    .eq('id', chat.id)
+    .throwOnError()
 
-//   return payload
-// }
+  return payload
+}
